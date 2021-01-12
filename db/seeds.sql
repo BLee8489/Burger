c4e329f6157f597069f34d2a7f8c8c7c36420b44
“bonnielee” <bonlee8489@gmail.com>
2021-01-11 17:47:44 -0800

    create files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Hamburger");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Mushroom Burger");
+INSERT INTO burgers (burger_name) VALUES ("Beyond Burger");
