7928edaca9717feecb06206e083f767f952eb7e9
“bonnielee” <bonlee8489@gmail.com>
2021-01-12 19:52:56 -0800

    EDITS: modified orm.js and controllers

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,75 @@
 const connection = require('./connection.js');
+
+// Helper function for SQL syntax to add question marks in query
+const printQuestionMarks = (num) => {
+	const arr = [];
+
+	for (let i = 0; i < num; i++) {
+		arr.push('?');
+	}
+
+	return arr.toString();
+};
+
+// Object for all our SQL statement functions.
+const orm = {
+	all(tableInput, cb) {
+		const queryString = `SELECT * FROM ${tableInput};`;
+		connection.query(queryString, (err, result) => {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
+	create(table, cols, vals, cb) {
+		let queryString = `INSERT INTO ${table}`;
+
+	queryString += ' (';
+    queryString += cols.toString();
+    queryString += ') ';
+    queryString += 'VALUES (';
+    queryString += printQuestionMarks(vals.length);
+    queryString += ') ';
+
+		connection.query(queryString, vals, (err, result) => {
+			if (err) {
+				throw err;
+			}
+
+			cb(result);
+		});
+	},
+	update(table, condition, cb) {
+		let queryString = `UPDATE ${table}`;
+
+		queryString += ' SET ';
+		queryString += 'devoured = 1';
+		queryString += ' WHERE ';
+		queryString += condition;
+		queryString += ';';
+
+		connection.query(queryString, (err, result) => {
+			if (err) {
+				throw err;
+			}
+
+			cb(result);
+		});
+	},
+	delete(table, condition, cb) {
+    let queryString = `DELETE FROM ${table}`;
+    queryString += ' WHERE ';
+    queryString += condition;
+        
+		connection.query(queryString, (err, result) => {
+			if (err) {
+				throw err;
+			}
+
+			cb(result);
+		});
+	},
+};
+
+module.exports = orm;
---
+++ controllers/burgerControllers.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+const burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get('/', (req, res) => {
+  burger.all((data) => {
+    const hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post('/api/burgers', (req, res) => {
+  burger.create(
+    ['burger_name', 'devoured'],
+    [req.body.burger_name, req.body.devoured],
+    (result) => {
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  console.log('condition', condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
+
+router.delete('/api/burgers', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+	burger.delete(condition, (result) => {
+        if (result.changedRows === 0) {
+			return res.status(404).end();
+		}
+		res.status(200).end();
+	});
+});
+
+
+module.exports = router;
